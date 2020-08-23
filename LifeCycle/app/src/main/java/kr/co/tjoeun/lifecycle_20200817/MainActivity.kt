476051ebacb23b89bae8442513779595c8bdb65f
jieunsys <jieunsys@naver.com>
2020-08-23 14:41:15 +0900

    LifeCycle init

---
+++ LifeCycle/app/src/main/java/kr/co/tjoeun/lifecycle_20200817/MainActivity.kt
@@ -0,0 +1,50 @@
+package kr.co.tjoeun.lifecycle_20200817
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
+        Log.d("메인화면", "onCreate 실행됨")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("메인화면", "onResume 실행됨 - 화면이 나타남")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("메인화면", "onPause 실행됨 - 화면이 사라짐")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("메인화면", "onDestory 실행됨 - 액티비티가 메모리에서 소멸됨")
+    }
+
+    override fun setupEvents() {
+
+        otherActivityBtn.setOnClickListener {
+            val myIntent = Intent(mContext, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
---
+++ LifeCycle/app/src/main/java/kr/co/tjoeun/lifecycle_20200817/OtherActivity.kt
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.lifecycle_20200817
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class OtherActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+        setupEvents()
+        setValues()
+
+        Log.d("다른화면", "onCreate 실행됨")
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
